20ec35868653bc6ee16e10e5002aa6260b15e02a
Tom Tamisier <tom.tamisier@tipee.ch>
2026-07-14 15:27:41 +0200

    add subscriptions route and tests (3.7.3.1)

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,13 @@
 //! tests/health_check.rs
 
+use std::any::TypeId;
+async fn spawn_app() -> String {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let address = format!("http://{}", listener.local_addr().unwrap());
+    tokio::spawn(zero2prod::run(listener));
+    address
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app().await;
@@ -16,9 +24,48 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> String {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
-    let address = format!("http://{}", listener.local_addr().unwrap());
-    tokio::spawn(zero2prod::run(listener));
-    address
+#[tokio::test]
+async fn subscribe_returns_200_valid_form_data() {
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_return_400_when_data_is_missing() {
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            "The API did not fail with 422 Unprocessable Entity when the payload was {}.",
+            error_message
+        )
+    }
 }
