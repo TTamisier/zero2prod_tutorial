34093d4ca821e355b04692ed40fbe2b1dda6e941
Tom Tamisier <tom.tamisier@tipee.ch>
2026-07-16 14:57:29 +0200

    Add Docker support and configuration files for application deployment

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -10,10 +10,15 @@ async fn main() {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failes to read configuration");
-    let pool = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
-    let addess = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool");
+
+    let addess = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = tokio::net::TcpListener::bind(addess).await.unwrap();
-    run(listener, pool).await
+    run(listener, connection_pool).await
 }
