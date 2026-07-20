0200543628ce78c5b22b9ef6ae22d6bf31361c98
Tom Tamisier <tom.tamisier@tipee.ch>
2026-07-20 11:47:15 +0200

    feat: implement email client for sending confirmation emails and update configuration

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use crate::email_client::EmailClient;
 use crate::routes::health_check;
 use crate::routes::subscribe;
 use axum::routing::post;
@@ -5,14 +6,25 @@ use axum::{Router, routing::get};
 use sqlx::PgPool;
 use tower_http::trace::TraceLayer;
 
-pub async fn run(listener: tokio::net::TcpListener, db_pool: PgPool) {
+#[derive(Clone)]
+pub struct AppState {
+    pub db_pool: PgPool,
+    pub email_client: EmailClient,
+}
+
+pub async fn run(listener: tokio::net::TcpListener, db_pool: PgPool, email_client: EmailClient) {
     tracing::info!("Starting server on {}", listener.local_addr().unwrap());
+    let state = AppState {
+        db_pool,
+        email_client,
+    };
+
     let app: Router = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
         .layer(TraceLayer::new_for_http())
-        .with_state(db_pool);
+        .with_state(state);
 
     axum::serve(listener, app).await.unwrap();
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -34,7 +34,17 @@ async fn spawn_app() -> TestApp {
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone());
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let email_client = zero2prod::email_client::EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
+
+    let server = run(listener, connection_pool.clone(), email_client);
 
     tokio::spawn(server);
 
